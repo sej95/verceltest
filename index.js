1333186b6312778757da3412a01132159da045d1
sej95 <144600333+sej95@users.noreply.github.com>
2025-02-22 20:17:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Vercel!');
+  res.send('Hello from Vercel! welcome test');
 });
 
 // Vercel 部署需要导出 Serverless 函数
